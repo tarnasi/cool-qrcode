3bfde36b55f25e2f07e8ab0feb262d418b6aa69d
tarnasi <shahryar.tarnasi@gmail.com>
2020-09-17 16:17:53 +0430

    project all done

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
+import io
+from base64 import b64encode
+import pyqrcode
 import eel
 
 # initial and Start project
 eel.init('web')
 
-
-
 @eel.expose
-def testFunc(Name):
-    print(Name)
+def generateQRCode(data):
+    img = pyqrcode.create(data)
+    buffers = io.BytesIO()
+    img.png(buffers, scale=8, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+    encoded = b64encode(buffers.getvalue()).decode("ascii")
+    print("QR Code Generation Successfull")
+    return "data:image/png;base64, " + encoded
 
-eel.start('main.html', size=(400, 400))
+eel.start('main.html', mode='firefox')
 
--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,15 @@
-eel.testFunc("Shahriyar")
+function generate()
+{
+    $("#loading").removeClass("d-none");
+    let textVal = document.getElementById('link').value;
+
+    setTimeout(() => {
+        eel.generateQRCode(textVal)(function(res) {
+            console.log("Image: ", res);
+            $("#loading").addClass("d-none");
+            $('#qrcode').removeClass('d-none')
+            $("#qrcode-image").attr("src", res)
+        })
+
+    }, 1000)
+}
